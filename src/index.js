1534184f4dc06d6436526ac4fe671c6165afe87a
Jodu555 <Jodu505@gmail.com>
2021-08-26 20:57:35 +0200

    Chnaged

--- src/index.js
+++ src/index.js
@@ -6,7 +6,51 @@ const cheerio = require('cheerio');
 
 const API_URL = 'https://www.youtube.com/watch?v=';
 
-downloadVideo('CppB6v12jls');
+const video = JSON.parse(fs.readFileSync('./firsttest.json'))[0];
+console.time('download')
+
+JSON.parse(fs.readFileSync('./firsttest.json')).forEach(async video => {
+    // console.log(video);
+    const id = video.snippet.resourceId.videoId;
+    const title = video.snippet.fileFriendlyTitle.replaceAll(' ', '_');
+    const thumbnail = video.snippet.thumbnails[Object.keys(video.snippet.thumbnails)[Object.keys(video.snippet.thumbnails).length - 1]];
+    const description = video.snippet.description;
+
+    fs.mkdirSync('downloads/' + title + '/');
+    await downloadVideo(id, title);
+    fs.writeFileSync('downloads/' + title + '/' + 'info.json', JSON.stringify(video, null, 3));
+    const response = await fetch(thumbnail.url);
+    response.body.pipe(fs.createWriteStream('downloads/' + title + '/thumbnail.png'))
+});
+
+console.timeEnd('download');
+
+async function downloadVideo(ID, name) {
+    const url = `${API_URL}${ID}`
+    let title = name;
+    if (!title)
+        title = await getVideoName(ID);
+
+    ytdl(url).pipe(fs.createWriteStream('downloads/' + name + '/' + 'video.mp4'));
+    console.log('Downloaded Video: \'' + title + '\'');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 async function getVideoName(ID) {
     const url = `${API_URL}${ID}`
@@ -19,10 +63,3 @@ async function getVideoName(ID) {
         });
     return title;
 }
-async function downloadVideo(ID) {
-    const url = `${API_URL}${ID}`
-    const title = await getVideoName(ID);
-    ytdl(url).pipe(fs.createWriteStream('video' + '.mp4'));
-    console.log('Downloaded Video: \'' + title + '\'');
-}
-
