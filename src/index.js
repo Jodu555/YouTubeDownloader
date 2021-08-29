197eb152d8f0c4dcf88e163960c9d53573c97f2d
Jodu555 <Jodu505@gmail.com>
2021-08-29 22:30:52 +0200

    Added Some Stuff to change to personal preference

--- src/index.js
+++ src/index.js
@@ -2,37 +2,48 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const { exit } = require('process');
 
 
 const API_URL = 'https://www.youtube.com/watch?v=';
 
-const video = JSON.parse(fs.readFileSync('./firsttest.json'))[0];
 console.time('download')
 
-JSON.parse(fs.readFileSync('./firsttest.json')).forEach(async video => {
+JSON.parse(fs.readFileSync('./abge.json')).forEach(async video => {
     // console.log(video);
-    const id = video.snippet.resourceId.videoId;
-    const title = video.snippet.fileFriendlyTitle.replaceAll(' ', '_');
-    const thumbnail = video.snippet.thumbnails[Object.keys(video.snippet.thumbnails)[Object.keys(video.snippet.thumbnails).length - 1]];
-    const description = video.snippet.description;
+    try {
+        const id = video.snippet.resourceId.videoId;
+        const title = video.snippet.fileFriendlyTitle.replaceAll(' ', '_');
+        const thumbnail = video.snippet.thumbnails[Object.keys(video.snippet.thumbnails)[Object.keys(video.snippet.thumbnails).length - 1]];
+        const description = video.snippet.description;
 
-    fs.mkdirSync('downloads/' + title + '/');
-    await downloadVideo(id, title);
-    fs.writeFileSync('downloads/' + title + '/' + 'info.json', JSON.stringify(video, null, 3));
-    const response = await fetch(thumbnail.url);
-    response.body.pipe(fs.createWriteStream('downloads/' + title + '/thumbnail.png'))
+        // console.log(await getVideoName(id));
+        fs.mkdirSync('downloads/' + title + '/');
+        await downloadVideo(id, title);
+        fs.writeFileSync('downloads/' + title + '/' + 'info.json', JSON.stringify(video, null, 3));
+        const response = await fetch(thumbnail.url);
+        response.body.pipe(fs.createWriteStream('downloads/' + title + '/thumbnail.png'))
+    } catch (error) {
+        console.error(error);
+        exit();
+    }
 });
 
 console.timeEnd('download');
 
 async function downloadVideo(ID, name) {
-    const url = `${API_URL}${ID}`
-    let title = name;
-    if (!title)
-        title = await getVideoName(ID);
+    try {
+        const url = `${API_URL}${ID}`
+        let title = name;
+        if (!title)
+            title = await getVideoName(ID);
+        await ytdl(url).pipe(fs.createWriteStream('downloads/' + name + '/' + 'video.mp4'));
+        console.log('Downloaded Video: \'' + title + '\' ' + ID + '');
+    } catch (error) {
+        console.log(ID, name);
+        throw error;
+    }
 
-    ytdl(url).pipe(fs.createWriteStream('downloads/' + name + '/' + 'video.mp4'));
-    console.log('Downloaded Video: \'' + title + '\'');
 }
 
 
