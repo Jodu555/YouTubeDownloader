ada10b9ad0762dd8fed6f18ddd44fe5835d2e2f2
Jodu555 <Jodu505@gmail.com>
2021-11-01 17:00:49 +0100

    fixes etc

--- src/index.js
+++ src/index.js
@@ -4,32 +4,46 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const { exit } = require('process');
 
-
 const API_URL = 'https://www.youtube.com/watch?v=';
+const output = 'downloads/'
 
-console.time('download')
+downloadVideo('8zpb-0_HxnQ');
 
-JSON.parse(fs.readFileSync('./abge.json')).forEach(async video => {
-    // console.log(video);
-    try {
-        const id = video.snippet.resourceId.videoId;
-        const title = video.snippet.fileFriendlyTitle.replaceAll(' ', '_');
-        const thumbnail = video.snippet.thumbnails[Object.keys(video.snippet.thumbnails)[Object.keys(video.snippet.thumbnails).length - 1]];
-        const description = video.snippet.description;
 
-        // console.log(await getVideoName(id));
-        fs.mkdirSync('downloads/' + title + '/');
-        await downloadVideo(id, title);
-        fs.writeFileSync('downloads/' + title + '/' + 'info.json', JSON.stringify(video, null, 3));
-        const response = await fetch(thumbnail.url);
-        response.body.pipe(fs.createWriteStream('downloads/' + title + '/thumbnail.png'))
-    } catch (error) {
-        console.error(error);
-        exit();
-    }
-});
 
-console.timeEnd('download');
+// console.time('download')
+
+
+
+// JSON.parse(fs.readFileSync('./abgeChunk.json')).forEach(async video => {
+//     // console.log(video);
+//     try {
+//         const id = video.snippet.resourceId.videoId;
+//         let title = video.snippet.fileFriendlyTitle.replaceAll(' ', '_');
+//         const thumbnail = video.snippet.thumbnails[Object.keys(video.snippet.thumbnails)[Object.keys(video.snippet.thumbnails).length - 1]];
+//         const description = video.snippet.description;
+//         // console.log(await getVideoName(id));
+//         try {
+//             fs.mkdirSync(output + title + '/');
+//         } catch (error) {
+//             if (error.stack.includes('file already exists')) {
+//                 title = title += '#' + Math.floor(Math.random() * 100);
+//                 fs.mkdirSync(output + title + '/');
+//             }
+//         }
+//         await downloadVideo(id, title);
+//         fs.writeFileSync(output + title + '/' + 'info.json', JSON.stringify(video, null, 3));
+//         const response = await fetch(thumbnail.url);
+//         await response.body.pipe(fs.createWriteStream(output + title + '/thumbnail.png'))
+
+//     } catch (error) {
+//         console.error(error);
+//         exit();
+//     }
+
+// });
+
+// console.timeEnd('download');
 
 async function downloadVideo(ID, name) {
     try {
@@ -37,7 +51,10 @@ async function downloadVideo(ID, name) {
         let title = name;
         if (!title)
             title = await getVideoName(ID);
-        await ytdl(url).pipe(fs.createWriteStream('downloads/' + name + '/' + 'video.mp4'));
+        if (!title)
+            title = ID;
+        await ytdl(url).pipe(fs.createWriteStream(output + '/' + ID + '.mp4'));
+        // await ytdl(url).pipe(fs.createWriteStream(output + ID + '/' + 'video.mp4'));
         console.log('Downloaded Video: \'' + title + '\' ' + ID + '');
     } catch (error) {
         console.log(ID, name);
@@ -48,21 +65,6 @@ async function downloadVideo(ID, name) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 async function getVideoName(ID) {
     const url = `${API_URL}${ID}`
     let title;
@@ -70,7 +72,7 @@ async function getVideoName(ID) {
         .then(res => res.text())
         .then(body => {
             const $ = cheerio.load(body);
-            title = $('title').html().replace(' - YouTube', '');
+            title = $('title').html();
         });
     return title;
 }
